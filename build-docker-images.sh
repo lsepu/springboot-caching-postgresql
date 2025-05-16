a9ec0beb4c9a6016784552d46c553a332d548959
lsepu9 <118215768+lsepu9@users.noreply.github.com>
2025-05-15 21:33:17 -0500

    Profile Configuration

---
+++ build-docker-images.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE_PREFIX="lsepu"
+APP_NAME="comic-api"
+APP_VERSION="1.0.0"
+DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${APP_NAME}:${APP_VERSION}"
+SKIP_TESTS="true"
+
+if [ "$1" = "native" ];
+then
+  ./mvnw -Pnative clean spring-boot:build-image \
+  -DskipTests="$SKIP_TESTS"
+  -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
+else
+  ./mvnw clean spring-boot:build-image
+  -DskipTests="$SKIP_TESTS"
+  -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
+fi
