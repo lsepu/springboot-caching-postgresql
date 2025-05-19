7ccb1ead6f51cf99188c713886c777dfd0fc9db0
lsepu9 <luiszy21@gmail.com>
2025-05-19 12:14:15 -0500

    Images added in documentation

--- src/main/java/com/comics/comic/configuration/SwaggerConfig.java
+++ src/main/java/com/comics/comic/configuration/SwaggerConfig.java
@@ -4,7 +4,6 @@ package com.comics.comic.configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,14 +19,14 @@ public class SwaggerConfig {
         return new OpenAPI().components(new Components()).info(new Info().title(applicationName));
     }
 
-    @Bean
+    /*@Bean
     GroupedOpenApi customApi() {
-        return GroupedOpenApi.builder().group("api").pathsToMatch("/api/**").build();
-    }
+        return GroupedOpenApi.builder().group("").pathsToMatch("/api/**").build();
+    }*/
 
-    @Bean
+    /*@Bean
     GroupedOpenApi actuatorApi() {
         return GroupedOpenApi.builder().group("actuator").pathsToMatch("/actuator/**").build();
-    }
+    }*/
 
 }
